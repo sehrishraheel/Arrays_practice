04a3c11821ec17e4b29ea55827f4153a517ab1dc
sehrishraheel <159688133+sehrishraheel@users.noreply.github.com>
2024-03-05 19:12:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// Arrays practice
+// string Array
+var vegetables = ["carrot", "potato", "onion"];
+vegetables[2] = "radish";
+console.log(vegetables);
+//Number Array
+var age = [22, 23, 24];
+console.log(24);
+// Boolean Arrays
+var color = true;
+console.log(color);
---
+++ app.ts
@@ -0,0 +1,15 @@
+// Arrays practice
+// string Array
+
+var vegetables: string[] = ["carrot", "potato", "onion"]
+vegetables[2] = "radish"
+console.log(vegetables)
+
+//Number Array
+var age: number[] = [22, 23 ,24]
+console.log(24)
+
+// Boolean Arrays
+
+var color: boolean = true
+console.log(color)
